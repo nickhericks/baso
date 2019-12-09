dfee0c532ffcbdca72b6290fbfc9aefbe6d900c4
Nick-Hericks <nickhericks@gmail.com>
2019-12-09 22:55:32 +0200

    Add js file

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let customers = [
+	{
+		name: 'Albert Rudatsimburwa',
+		tweet: 'Finally I made it… @baso_patissier the Artist… viennoisery, pastry #MadeInRwanda of another league… rarely seen & tasted anything alike'
+	},
+	{
+		name: 'Adam Kyamatare',
+		tweet: 'Round 2 of pastries at @baso_patissier is even bloodier. These New Years resolutions are looking awfully unfamiliar… #RwOT'
+	},
+	{
+		name: 'Regis Isheja',
+		tweet: 'Tasting at @baso_patissier! Their signature chocolate “Matadi” is EVERYTHING! Sweet tooth heaven. SUPPORT YOUR OWN! #Rwanda #RwOT'
+	},
+	{
+		name: 'Fiona Mbabazi',
+		tweet: '@baso_patissier awesome stuff!! Thank you for saving the sweet tooth'
+	},
+	{
+		name: 'Linda Mukangoga',
+		tweet: 'Heaven @baso_patissier | Made in Rwanda'
+	}
+];
+
+const carousel = document.querySelector(".carousel");
+
+
+for(customer of customers) {
+	
+}
